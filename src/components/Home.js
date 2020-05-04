b6d01578c45f7d08c6856be57d97feb77be0cb6b
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2020-05-04 19:17:09 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,35 @@
-import React,{useRef} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import {Dropdown} from "semantic-ui-react";
 
 export const Home = () => {
     const mform = useRef(null);
     let handleSubmit = (e) => {
         console.log(mform.current, mform.current.childNodes);
         e.preventDefault();
+    }
+    let semantic_drop_down_check = () => {
+        useEffect(()=>{
+            setCurSel('three');
+        },[]);
+        let [cursel, setCurSel] = useState('');
+        let dropdown1 = [
+            {id: 1, text: 'one', value: 'one', key: 1}
+            , {id: 2, text: 'two', value: 'two', key: 2}
+            , {id: 2, text: 'three', value: 'three', key: 3}
+        ];
+        let onChangeSel = (e, {name, value}) => {
+            setCurSel(value);
+            console.log('data change', name, value);
+        }
+        return (<div>
+            <h1>semantic ui dropdown</h1>
+            <Dropdown
+                name='dropdown1'
+                options={dropdown1}
+                value={cursel}
+                onChange={onChangeSel}
+            />
+        </div>);
     }
     return (
         <div>
@@ -20,6 +45,8 @@ export const Home = () => {
             <h1 className="btn orange" onClick={() => {
                 alert('I am loving it - wondering how cool it is')
             }}>Final Click</h1>
+
+            {semantic_drop_down_check()}
         </div>
     )
 }
