5a50c54e085eaa7f45e38ceabe61d419462f1720
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2020-05-07 22:21:35 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {useRef, useState, useEffect} from 'react';
 import {Dropdown} from "semantic-ui-react";
+import {success, error, warning} from 'toastr';
 
 export const Home = () => {
     const mform = useRef(null);
@@ -8,9 +9,9 @@ export const Home = () => {
         e.preventDefault();
     }
     let semantic_drop_down_check = () => {
-        useEffect(()=>{
+        useEffect(() => {
             setCurSel('three');
-        },[]);
+        }, []);
         let [cursel, setCurSel] = useState('');
         let dropdown1 = [
             {id: 1, text: 'one', value: 'one', key: 1}
@@ -31,6 +32,49 @@ export const Home = () => {
             />
         </div>);
     }
+
+    let promiseCheck = () => {
+        const [data, setData] = useState({});
+        const [loaded, setLoaded] = useState(false);
+        useEffect(() => {
+            if (loaded) return;
+            handlePromiseData();
+            setLoaded(true);
+        }, [loaded]);
+
+        // useEffect(() => {
+        //     if (typeof data === "undefined" || Object.keys(data).length === 0) return;
+        //     console.log('data loaded', data);
+        //     success('data is loaded via promises');
+        // }, [data]);
+
+        let handlePromiseData = () => {
+            console.log('inside promise');
+            let alldata = {};
+            for (let i = 1; i < 10; i++) {
+                fetch('https://jsonplaceholder.typicode.com/todos/' + i)
+                    .then(res => res.json())
+                    .then(res => {
+                        alldata[i] = res;
+                        setData(alldata);
+                    })
+            }
+        }
+        let checkAfterClick = () => {
+            console.log('data state from promise', data)
+            for(let i in data){
+                console.log('loading from data',i,data[i]);
+            }
+        }
+        return (
+            <div>
+                <h1>promise checks</h1>
+                <button className='btn green' onClick={() => handlePromiseData()}>click promise checks</button>
+                <button className='btn yellow' onClick={() => checkAfterClick()}>check state</button>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h1 className="ribbon">Home Contents - OK</h1>
@@ -47,6 +91,7 @@ export const Home = () => {
             }}>Final Click</h1>
 
             {semantic_drop_down_check()}
+            {promiseCheck()}
         </div>
     )
 }
