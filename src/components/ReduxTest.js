d23d62e1aa9a1ce6d6992ae0c4242ffdf0309c2d
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2020-05-09 19:15:27 +0530

    update

--- src/components/ReduxTest.js
+++ src/components/ReduxTest.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import {createStore, combineReducers} from 'redux';
 import {visibilityFilter, todos} from '../Redux/AppReducers';
-import {addTodo, completeAction} from "../Redux/AppActions";
+import {addTodo} from "../Redux/AppActions";
 
 
 //everything is js is an object, you can assign properties to it, they can be of any type
 //their must be one global state in the application
 export const ReduxTest = () => {
-    let todoAppReducer = combineReducers({visibilityFilter, todos});
     // export default function todoApp(state = {}, action) {
+    let [users, setUsers] = useState([]);
+    let [canPrint, setCanPrint] = useState(false);
+    let todoAppReducer = combineReducers({visibilityFilter, todos});
     //     return {
     //         visibilityFilter: visibilityFilter(state.visibilityFilter, action),
     //         todos: todos(state.todos, action)
@@ -17,8 +19,8 @@ export const ReduxTest = () => {
     let store = createStore(todoAppReducer);
     // Note that subscribe() returns a function for unregistering the listener
     const unsubscribe = store.subscribe(() => console.log('changing state', store.getState())); //on every state change
-    let [users, setUsers] = useState([]);
     let changeVisibility = () => {
+        setCanPrint(false);
         store.dispatch({type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_ALL'});
         store.dispatch({type: 'SET_VISIBILITY_FILTER', filter: 'DONT_RUN_AND_DISABLE'});
     }
@@ -34,6 +36,13 @@ export const ReduxTest = () => {
     let markAllComplete = () => {
         store.dispatch({type: 'COMPLETE_TODO',index:1});
         store.dispatch({type: 'COMPLETE_TODO_ALL',index:-1000});
+        setCanPrint(true);
+    }
+    let printListing=()=>{
+        if(!canPrint) return;
+        let todos=store.getState().todos;
+        console.log('inside printing',todos);
+        return todos.map(x=><div>{x}</div>);
     }
     return (
         <div>
@@ -41,7 +50,11 @@ export const ReduxTest = () => {
             <button className='btn' onClick={() => changeVisibility()}>change visibility of todo</button>
             <button className='btn' onClick={() => todoDispatch()}>call todo dispatcher</button>
             <button className='btn' onClick={() => markAllComplete()}>mark all complete</button>
+            <button className='btn blue' onClick={() => setCanPrint(true)}>PRINT</button>
             <button className='btn red' onClick={() => stopTrackingChanges()}>Stop State Tracking</button>
+            <h1>todo listing...</h1>
+            {printListing()}
         </div>
     )
 }
+
